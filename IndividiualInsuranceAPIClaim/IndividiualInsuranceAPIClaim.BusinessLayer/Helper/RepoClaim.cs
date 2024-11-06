e3be9232f1060a3bf62b0ec727704ec7e5692e44
adamsuherman11 <adam.suherman@inhealth.co.id>
2024-11-06 10:11:03 +0700

    validasi plafond

--- IndividiualInsuranceAPIClaim/IndividiualInsuranceAPIClaim.BusinessLayer/Helper/RepoClaim.cs
+++ IndividiualInsuranceAPIClaim/IndividiualInsuranceAPIClaim.BusinessLayer/Helper/RepoClaim.cs
@@ -1,6 +1,8 @@
 ﻿using IndividiualInsuranceAPIClaim.BusinessLayer.BindingModel;
 using IndividiualInsuranceAPIClaim.DataAccess.Context;
 using IndividiualInsuranceAPIClaim.DataAccess.Models.Claim;
+using IndividiualInsuranceAPIClaim.DataAccess.Models.Membership;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,34 +27,61 @@ namespace IndividiualInsuranceAPIClaim.BusinessLayer.Helper
             BResInsertClaim result = new BResInsertClaim();
             Guid id = Guid.NewGuid();
 
-            trClaim param = new trClaim();
-            param.ID = id;
-            param.CardNumber = request.CardNumber;
-            param.DiagonosaCode = request.DiagonosaCode;
-            param.Cost = request.Cost;
-            param.TreatmentDate = request.TreatmentDate;
-            param.CreatedBy = request.CreatedBy;
-            param.CreatedDate = request.CreatedDate;
-            _context.Add(param);
-            _context.SaveChanges();
+            var plafond = _membershipContext.TblPeserta.Where(s => s.CardNumber == request.CardNumber).Select(s => s.Plafond).FirstOrDefault();
 
-            var data = (from claim in _context.trClaim
-                        join diagnosa in _context.msDiagnosa on claim.DiagonosaCode equals diagnosa.DiagonosaCode
-                        where claim.ID == id
-                        select new
-                        {
-                            ID = claim.ID,
-                            CardNumber = claim.CardNumber,
-                            Diagnosa = diagnosa.DiagonsaName,
-                            Cost = claim.Cost,
-                            TreatmentDate = claim.TreatmentDate,
-                            CreatedBy = claim.CreatedBy,
-                            CreatedDate = claim.CreatedDate
-                        }).FirstOrDefault();
+            if(Convert.ToInt32(plafond) > Convert.ToInt32(request.Cost))
+            {
+                trClaim param = new trClaim();
+                param.ID = id;
+                param.CardNumber = request.CardNumber;
+                param.DiagonosaCode = request.DiagonosaCode;
+                param.Cost = request.Cost;
+                param.TreatmentDate = request.TreatmentDate;
+                param.CreatedBy = request.CreatedBy;
+                param.CreatedDate = request.CreatedDate;
+                _context.Add(param);
 
-            result.ResultCode = "01";
-            result.ResultMessage = "Save Successfully";
-            result.Data = data;
+
+                var existingData = _membershipContext.TblPeserta.Where(t => t.CardNumber == request.CardNumber).FirstOrDefault();
+                if(existingData != null)
+                {
+                    decimal newPlafond = Convert.ToDecimal(existingData.Plafond) - Convert.ToDecimal(request.Cost);
+                    //var peserta = new TblPeserta { Plafond = newPlafond };
+                    //_membershipContext.Entry(existingData).CurrentValues.SetValues(peserta);
+                    (from p in _membershipContext.TblPeserta
+                     where p.CardNumber == request.CardNumber
+                     select p)
+                     .ToList()
+                     .ForEach(x => x.Plafond = newPlafond);
+                    
+                }
+                _context.SaveChanges();
+                _membershipContext.SaveChanges();
+
+                var data = (from claim in _context.trClaim
+                            join diagnosa in _context.msDiagnosa on claim.DiagonosaCode equals diagnosa.DiagonosaCode
+                            where claim.ID == id
+                            select new
+                            {
+                                ID = claim.ID,
+                                CardNumber = claim.CardNumber,
+                                Diagnosa = diagnosa.DiagonsaName,
+                                Cost = claim.Cost,
+                                TreatmentDate = claim.TreatmentDate,
+                                CreatedBy = claim.CreatedBy,
+                                CreatedDate = claim.CreatedDate
+                            }).FirstOrDefault();
+
+                result.ResultCode = "01";
+                result.ResultMessage = "Save Successfully";
+                result.Data = data;
+            }
+            else
+            {
+                result.ResultCode = "00";
+                result.ResultMessage = "Save Failed, Limit anda tidak cukup.";
+                result.Data = null;
+            }
 
             return result;
         }
